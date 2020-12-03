63f7ec1aeacb175778201912749823b87448b3fb
jim4067 <jimmyimpulse2@gmail.com>
2020-12-03 10:58:36 +0300

    feat: and then there was code and scripts to run it

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (_req, res) => {
+    res.send("pong ");
+});
+
+const PORT = 3030;
+
+app.listen(PORT, () => {
+    console.log(`successfully running the server on port ${PORT}`);
+});
